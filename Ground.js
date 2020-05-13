d1bdd6b03c29909a413bd22b35b03292ad90e4e2
Ekansh Sharma <53142357+teendevlper@users.noreply.github.com>
2020-05-13 13:47:43 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,18 @@
+class Ground {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.image = loadImage("sprites/ground.png")
+    }
+    display(){
+      push();
+      imageMode(CENTER);
+      image(this.image,this.body.position.x,this.body.position.y,this.width,this.height);
+      pop();
+    }
+  };
